e7918e1954d91a8b1e28721fea00608c10b82816
YounessBird <67457600+YounessBird@users.noreply.github.com>
2022-07-07 23:55:33 +0100

    business logic

--- Pages/Forms/CustomPizza.cshtml.cs
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -7,8 +7,23 @@ public class CustomPizzaModel : PageModel
 {
     [BindProperty]
     public PizzasModel Pizza { get; set; }
-
+    public float PizzaPrice { get; set; }
     public void OnGet()
     {
+    }
+
+    public IActionResult OnPost()
+    {
+
+        PizzaPrice = Pizza.BasePrice;
+        if (Pizza.TomatoSauce) { ++PizzaPrice; }
+        if (Pizza.Cheese) { ++PizzaPrice; }
+        if (Pizza.Beef) { PizzaPrice += 3; }
+        if (Pizza.Pineapple) { PizzaPrice += 2; }
+        if (Pizza.Peperoni) { PizzaPrice += 4; }
+        if (Pizza.Tuna) { PizzaPrice += 9; }
+        if (Pizza.Ham) { PizzaPrice += 2; }
+
+        return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
     }
 }
